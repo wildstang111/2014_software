4da9142c452720270dcfd2663287a29f59fe7cbc
Jason <Jason@Jasons.d214.org>
2014-01-30 19:34:14 -0600

    Created Preset Framework

--- cRioTarget/src/com/wildstangs/subsystems/BallHandler.java
+++ cRioTarget/src/com/wildstangs/subsystems/BallHandler.java
@@ -9,6 +9,7 @@ import com.wildstangs.subjects.base.DoubleSubject;
 import com.wildstangs.subjects.base.IObserver;
 import com.wildstangs.subjects.base.Subject;
 import com.wildstangs.subsystems.arm.Arm;
+import com.wildstangs.subsystems.arm.ArmPreset;
 import com.wildstangs.subsystems.base.WsSubsystem;
 import edu.wpi.first.wpilibj.Relay;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -18,81 +19,70 @@ import java.util.Random;
  *
  * @author mail929
  */
-public class BallHandler extends WsSubsystem implements IObserver
-{
-    
+public class BallHandler extends WsSubsystem implements IObserver {
+
     protected Arm frontArm, backArm;
     protected boolean frontForwardButton = false, frontReverseButton = false;
     protected boolean backForwardButton = false, backReverseButton = false;
     protected double frontArmJoystickValue = 0.0, backArmJoystickValue = 0.0;
     Random rnd = new Random();
     int[] positions = {10, 100, 200, 350, 50, 90, 30, 120, 160, 212, 208, 240};
-    
-    public BallHandler(String name) 
-    {
+
+    public BallHandler(String name) {
         super(name);
-        
+
         this.frontArm = new Arm(WsOutputManager.FRONT_ARM_VICTOR_INDEX, WsOutputManager.FRONT_ARM_SPIKE_INDEX, WsInputManager.FRONT_ARM_POT_INDEX, true);
         this.backArm = new Arm(WsOutputManager.BACK_ARM_VICTOR_INDEX, WsOutputManager.BACK_ARM_SPIKE_INDEX, WsInputManager.BACK_ARM_POT_INDEX, false);
-        
+
         registerForJoystickButtonNotification(WsJoystickButtonEnum.MANIPULATOR_BUTTON_5);
         registerForJoystickButtonNotification(WsJoystickButtonEnum.MANIPULATOR_BUTTON_6);
         registerForJoystickButtonNotification(WsJoystickButtonEnum.MANIPULATOR_BUTTON_7);
         registerForJoystickButtonNotification(WsJoystickButtonEnum.MANIPULATOR_BUTTON_8);
         registerForJoystickButtonNotification(WsJoystickButtonEnum.MANIPULATOR_BUTTON_2);
+        registerForJoystickButtonNotification(WsJoystickButtonEnum.MANIPULATOR_BUTTON_1);
+        registerForJoystickButtonNotification(WsJoystickButtonEnum.MANIPULATOR_BUTTON_4);
+        registerForJoystickButtonNotification(WsJoystickButtonEnum.MANIPULATOR_BUTTON_3);
         
         Subject subject = WsInputManager.getInstance().getOiInput(WsInputManager.MANIPULATOR_JOYSTICK_INDEX).getSubject(WsJoystickAxisEnum.MANIPULATOR_BACK_ARM_CONTROL);
         subject.attach(this);
-        
+
         subject = WsInputManager.getInstance().getOiInput(WsInputManager.MANIPULATOR_JOYSTICK_INDEX).getSubject(WsJoystickAxisEnum.MANIPULATOR_FRONT_ARM_CONTROL);
         subject.attach(this);
     }
 
-    public void init() 
-    {
+    public void init() {
     }
 
-    public void update() 
-    {
+    public void update() {
         //Both pressed or both are not pressed
-        if(frontForwardButton == frontReverseButton) 
-        {
+        if (frontForwardButton == frontReverseButton) {
             frontArm.setRoller(Relay.Value.kOff);
-        }
-        else if(frontForwardButton) 
-        {
+        } else if (frontForwardButton) {
             frontArm.setRoller(Relay.Value.kForward);
-        }
-        else if(frontReverseButton) 
-        {
+        } else if (frontReverseButton) {
             frontArm.setRoller(Relay.Value.kReverse);
         }
-        
+
         //Both pressed or both are not pressed
-        if(backForwardButton == backReverseButton)
-        {
+        if (backForwardButton == backReverseButton) {
             backArm.setRoller(Relay.Value.kOff);
-        }
-        else if(backForwardButton) 
-        {
+        } else if (backForwardButton) {
             backArm.setRoller(Relay.Value.kForward);
-        }
-        else if(backReverseButton) 
-        {
+        } else if (backReverseButton) {
             backArm.setRoller(Relay.Value.kReverse);
         }
-        
+
         frontArm.setVictor(frontArmJoystickValue);
         backArm.setVictor(backArmJoystickValue);
-        
+
         frontArm.update();
         backArm.update();
-        
+
         Relay.Value frontValue = frontArm.getRollerValue();
         Relay.Value backValue = backArm.getRollerValue();
         String frontString = frontValue == Relay.Value.kForward ? "Forward" : (frontValue == Relay.Value.kReverse ? "Reverse" : "Off");
         String backString = backValue == Relay.Value.kForward ? "Forward" : (backValue == Relay.Value.kReverse ? "Reverse" : "Off");
-        
+
         SmartDashboard.putNumber("Current Front Arm Angle", frontArm.getCurrentAngle());
         SmartDashboard.putNumber("Wanted Front Arm Angle", frontArm.getWantedAngle());
         SmartDashboard.putNumber("Current Back Arm Angle", backArm.getCurrentAngle());
@@ -101,45 +91,50 @@ public class BallHandler extends WsSubsystem implements IObserver
         SmartDashboard.putString("Back Arm Roller", backString);
         SmartDashboard.putNumber("Front Arm Victor", frontArm.getVictorSpeed());
         SmartDashboard.putNumber("Back Arm Victor", backArm.getVictorSpeed());
+        
     }
 
-    public void notifyConfigChange() 
-    {
+    public void notifyConfigChange() {
         frontArm.notifyConfigChange();
         backArm.notifyConfigChange();
     }
 
-    public void acceptNotification(Subject subjectThatCaused)
-    {
-        if(subjectThatCaused.getType() == WsJoystickButtonEnum.MANIPULATOR_BUTTON_5)
-        {
+    public void acceptNotification(Subject subjectThatCaused) {
+        if (subjectThatCaused.getType() == WsJoystickButtonEnum.MANIPULATOR_BUTTON_5) {
             this.frontForwardButton = ((BooleanSubject) subjectThatCaused).getValue();
-        }
-        else if(subjectThatCaused.getType() == WsJoystickButtonEnum.MANIPULATOR_BUTTON_6)
-        {
+        } else if (subjectThatCaused.getType() == WsJoystickButtonEnum.MANIPULATOR_BUTTON_6) {
             this.backForwardButton = ((BooleanSubject) subjectThatCaused).getValue();
-        }
-        else if(subjectThatCaused.getType() == WsJoystickButtonEnum.MANIPULATOR_BUTTON_7)
-        {
+        } else if (subjectThatCaused.getType() == WsJoystickButtonEnum.MANIPULATOR_BUTTON_7) {
             this.frontReverseButton = ((BooleanSubject) subjectThatCaused).getValue();
-        }
-        else if(subjectThatCaused.getType() == WsJoystickButtonEnum.MANIPULATOR_BUTTON_8)
-        {
+        } else if (subjectThatCaused.getType() == WsJoystickButtonEnum.MANIPULATOR_BUTTON_8) {
             this.backReverseButton = ((BooleanSubject) subjectThatCaused).getValue();
-        }
-        else if(subjectThatCaused.getType() == WsJoystickAxisEnum.MANIPULATOR_FRONT_ARM_CONTROL)
-        {
+        } else if (subjectThatCaused.getType() == WsJoystickAxisEnum.MANIPULATOR_FRONT_ARM_CONTROL) {
             this.frontArmJoystickValue = ((DoubleSubject) subjectThatCaused).getValue();
-        }
-        else if(subjectThatCaused.getType() == WsJoystickAxisEnum.MANIPULATOR_BACK_ARM_CONTROL)
-        {
+        } else if (subjectThatCaused.getType() == WsJoystickAxisEnum.MANIPULATOR_BACK_ARM_CONTROL) {
             this.backArmJoystickValue = ((DoubleSubject) subjectThatCaused).getValue();
-        }
-        else if(subjectThatCaused.getType() == WsJoystickButtonEnum.MANIPULATOR_BUTTON_2)
-        {
-            int position = positions[rnd.nextInt(positions.length)];
-            this.frontArm.setToAngle(position);
-            SmartDashboard.putNumber("Setting to position", position);
+        } else if (subjectThatCaused.getType() == WsJoystickButtonEnum.MANIPULATOR_BUTTON_2) {
+            this.setArmPreset(armPresetTest);
+        } else if (subjectThatCaused.getType() == WsJoystickButtonEnum.MANIPULATOR_BUTTON_3) {
+            this.setArmPreset(armPresetTestTwo);
+        } else if (subjectThatCaused.getType() == WsJoystickButtonEnum.MANIPULATOR_BUTTON_4) {
+            this.setArmPreset(armPresetTestThree);
+            } else if (subjectThatCaused.getType() == WsJoystickButtonEnum.MANIPULATOR_BUTTON_1) {
+            this.setArmPreset(armPresetTestFour);
         }
     }
+
+    public void setArmPreset(ArmPreset preset) {
+        int frontArmPreset = preset.getwantedAngleMeasureFront();
+        int backArmPreset = preset.getwantedAngleMeasureBack();
+        if (frontArmPreset <= 359 && frontArmPreset >= 0) {
+            this.frontArm.setToAngle(frontArmPreset);
+        }
+        if (backArmPreset <= 359 && backArmPreset>=0){
+            this.backArm.setToAngle(backArmPreset);
+        }
+    }
+    ArmPreset armPresetTest = new ArmPreset(112, -100);
+    ArmPreset armPresetTestTwo = new ArmPreset(45, 45);
+    ArmPreset armPresetTestThree = new  ArmPreset(-100, -100);
+    ArmPreset armPresetTestFour = new  ArmPreset(-100, 100);
 }
