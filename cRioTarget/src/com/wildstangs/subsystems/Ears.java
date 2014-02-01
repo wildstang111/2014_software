b23f2d0a641ea26137d1519a213b7df31aa2dced
Sahil <Sahil@10.71.8.115>
2014-02-01 10:34:51 -0600

    The Ears Subsystem

--- cRioTarget/src/com/wildstangs/subsystems/Ears.java
+++ cRioTarget/src/com/wildstangs/subsystems/Ears.java
@@ -1,7 +1,4 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.wildstangs.subsystems;
 
 import com.wildstangs.inputmanager.inputs.joystick.WsJoystickButtonEnum;
