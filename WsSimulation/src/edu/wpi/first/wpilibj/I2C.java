b19763dc4b6065f58d6272ae0a871accf363222d
Chris Smith <slider6791@gmail.com>
2014-02-08 17:14:34 -0600

    Simplify LED subsystem Update Simulation to display LED commands.

--- WsSimulation/src/edu/wpi/first/wpilibj/I2C.java
+++ WsSimulation/src/edu/wpi/first/wpilibj/I2C.java
@@ -4,6 +4,8 @@
  */
 package edu.wpi.first.wpilibj;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 /**
  *
  * @author Nathan
@@ -14,6 +16,8 @@ public class I2C {
     }
 
     public synchronized boolean transaction(byte[] dataToSend, int sendSize, byte[] dataReceived, int receiveSize) {
+        SmartDashboard.putString("LED Data:", "C:0x" + Integer.toHexString((int)dataToSend[0]) + " D1:0x" +
+                Integer.toHexString((int)dataToSend[1]) + " D2:0x" + Integer.toHexString((int)dataToSend[2]));
         return false;
     }
 }
